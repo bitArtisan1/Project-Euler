068866dd8a3ba9d30c2fb364fd8922ba75d3a620
yanpuri <121260820+yanpuri@users.noreply.github.com>
2023-05-19 18:00:14 +0100

    Update (Problem 14) Longest Collatz sequence.cpp

--- (Problem 14) Longest Collatz sequence.cpp
+++ (Problem 14) Longest Collatz sequence.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int collatz_chain(long long n){
 	
     int length = 1;
-    while (n != 1) {
+    while (n != 1){
         if (n % 2 == 0)
             n = n / 2;
         else
             n = 3 * n + 1;
-            
+     
         length++;
     }
     return length;
